2d35a27d0204b66ecdb5e6c354f45d432847a488
UltralyticsAssistant <web@ultralytics.com>
2026-04-24 04:32:46 +0000

    Auto-format by https://ultralytics.com/actions

--- trainyolobiasa.py
+++ trainyolobiasa.py
@@ -5,15 +5,15 @@ model = YOLO("yolo11l.pt")  # Memperbarui konfigurasi model dengan file YAML yan
 
 # Memulai proses pelatihan dengan parameter dari paper referensi
 model.train(
-    data="data.yaml",         # Ganti dengan path ke file dataset YAML kamu
-    epochs=60,                # Jumlah epoch
-    imgsz=640,                # Resolusi gambar input
-    batch=16,                 # Ukuran batch
-    optimizer="Adam",         # Optimizer
-    lr0=0.001,                # Initial learning rate
-    device=0,                 # Gunakan GPU 0
-    project="yolo11_project", # Direktori utama penyimpanan hasil
-    name="train_baseline"     # Nama sub-direktori run ini
+    data="data.yaml",  # Ganti dengan path ke file dataset YAML kamu
+    epochs=60,  # Jumlah epoch
+    imgsz=640,  # Resolusi gambar input
+    batch=16,  # Ukuran batch
+    optimizer="Adam",  # Optimizer
+    lr0=0.001,  # Initial learning rate
+    device=0,  # Gunakan GPU 0
+    project="yolo11_project",  # Direktori utama penyimpanan hasil
+    name="train_baseline",  # Nama sub-direktori run ini
 )
 
 print("Pelatihan selesai!")
