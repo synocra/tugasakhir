110786007afceb7f50a34d2c8a07a34aa5b655d6
synocra <mukhlisnurhidayat09@gmail.com>
2026-04-26 04:27:54 +0700

    Ubah Dataset

---
+++ ultralytics/nn/modules/coord_att.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        mip = max(8, inp // reduction)
+
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = nn.ReLU()
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1)
+
+    def forward(self, x):
+        identity = x
+        n, c, h, w = x.size()
+
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.act(self.bn1(self.conv1(y)))
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = torch.sigmoid(self.conv_h(x_h))
+        a_w = torch.sigmoid(self.conv_w(x_w))
+
+        out = identity * a_h * a_w
+        return out
